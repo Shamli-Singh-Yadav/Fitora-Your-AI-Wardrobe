fcadaa5e4dfcd25dc8aaf3736d1e3636320e77c6
Shamli-Singh-Yadav <shamlisinghyadav@gmail.com>
2026-02-14 11:28:05 +0530

    home page icons added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
+import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
 
